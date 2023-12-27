2dc703c425ec20f07b0350e11a3658f88f322aa9
z0l0git <zolbayar.in@gmail.com>
2023-12-27 12:36:39 +0800

    icons responsive finished

--- portfolio/src/components/SkillsPage.jsx
+++ portfolio/src/components/SkillsPage.jsx
@@ -34,6 +34,38 @@ const coms = [
     component: "IconContent",
     params: { image: "/icon-socket.png", text: "Socket.io" },
   },
+  {
+    component: "IconContent",
+    params: { image: "/icon-postgresql.png", text: "PostgreSQL" },
+  },
+  {
+    component: "IconContent",
+    params: { image: "/icon-mongodb.png", text: "MongoDB", width: "29px" },
+  },
+  {
+    component: "IconContent",
+    params: { image: "/icon-sass.png", text: "Sass/Scss" },
+  },
+  {
+    component: "IconContent",
+    params: { image: "/icon-tailwindcss.png", text: "Tailwind" },
+  },
+  {
+    component: "IconContent",
+    params: { image: "/icon-figma.png", text: "Figma" },
+  },
+  {
+    component: "IconContent",
+    params: { image: "/icon-cypress.png", text: "Cypress" },
+  },
+  {
+    component: "IconContent",
+    params: { image: "/icon-storybook.png", text: "Storybook" },
+  },
+  {
+    component: "IconContent",
+    params: { image: "/icon-git.png", text: "Git" },
+  },
 ];
 const coms2 = [
   {
@@ -72,7 +104,7 @@ const coms2 = [
 
 export const SkillsPage = () => {
   return (
-    <div className="w-full h-fit px-[80px] py-[96px]">
+    <div className="w-full h-fit px-[80px] py-[96px] flex flex-col items-center">
       <div className="flex items-center justify-center w-full mb-[16px]">
         <Tag text="Skills"></Tag>
       </div>
@@ -81,24 +113,12 @@ export const SkillsPage = () => {
           The skills, tools and technologies I am really good at:
         </p>
       </div>
-      <div className="mx-[32px] flex flex-col gap-[48px]">
-        <div className="flex w-full justify-between ">
+      <div className=" w-[343px] md:w-full mx-[32px] px-[16px] md:px-[32px] flex flex-col flex-wrap gap-[16px] md:gap-[48px] items-center justify-center">
+        <div className="flex w-[300px] md:w-full justify-between flex-wrap gap-y-[48px] gap-x-[20px] md:gap-x-[100px]">
           {coms.map((com, i) => {
             return (
               <IconContent
                 key={i}
-                image={com.params.image}
-                text={com.params.text}
-              />
-            );
-          })}
-        </div>
-        <div className="flex w-full justify-between ">
-          {coms2.map((com, i) => {
-            return (
-              <IconContent
-                key={i}
-                width={com.params.width}
                 image={com.params.image}
                 text={com.params.text}
               />
